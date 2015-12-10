c1db7f2cf28cbbbbf574b206fb7119cc696ae93e
nbayashi <naoya_nstyle@hotmail.co.jp>
2015-12-11 01:57:12 +0900

    cesium2

---
+++ cesium2/cesium_line.js
@@ -0,0 +1,13 @@
+var viewer = new Cesium.Viewer('cesiumContainer');
+var line1 = viewer.entities;
+line1.add({
+     polyline : {
+         positions : Cesium.Cartesian3.fromDegreesArrayHeights([
+-75 , 35 , 200000 ,
+ 123 , 45 , 3000000 ,
+ 135 , 40 , 0 ,
+ 20 , 80 , 40000000 
+]),
+         width : 5,
+         material : new Cesium.PolylineArrowMaterialProperty(Cesium.Color.BLUE)
+     }});
