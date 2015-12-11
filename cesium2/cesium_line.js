29e07b8f8edb2cdfb329fb01dfd6f325f10593fb
bayashi <naoya_nstyle@hotmail.co.jp>
2015-12-12 02:21:40 +0900

    Update cesium_line.js

--- cesium2/cesium_line.js
+++ cesium2/cesium_line.js
@@ -4,7 +4,7 @@ line1.add({
      polyline : {
          positions : Cesium.Cartesian3.fromDegreesArrayHeights([
  139.632509 , 35.68453 , 200000 ,
--122.555284 , 38.058506 , 200000 
+ 22.555284 , 38.058506 , 200000 
 ]),
          width : 10,
          material : new Cesium.PolylineArrowMaterialProperty(Cesium.Color.BLUE)
